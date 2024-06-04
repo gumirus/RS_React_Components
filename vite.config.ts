7113c204980196c6fce266bd6a9672191d403202
αγόρι <agori@Mac-Pro-agori.local>
2024-06-04 17:08:50 +0500

    fix:correction

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/RS_React_Components/',
+  base: '/',
   plugins: [react()],
 });
